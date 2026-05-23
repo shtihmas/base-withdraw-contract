f7683491fa9b248f8e3c40485f89486d21d38a4e
Shtihmas <shtihmas@MacBook-Air-Shtihmas.local>
2026-05-23 11:39:07 +0300

    chore: update contract - commit 3

--- Contract.sol
+++ Contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 contract BaseContract {
     string public name = "";
-    uint256 public commitCount = 2;
+    uint256 public commitCount = 3;
     uint256 public lastUpdated = block.timestamp;
 
     event ContractUpdated(uint256 commitCount);
